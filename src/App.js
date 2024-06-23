ffbd6fb08fc416f715040b2b6d9118467b15e363
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-06-23 21:31:33 +0400

    Update layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import {MediaPlayer} from "./MediaPlayer";
 import {SoundContext} from "./contexts/soundContext";
 import {SongCard} from "./components/SongCard";
 import {config} from "./config";
-
-
+import {HomeOutlined, Search} from "@mui/icons-material";
 
 
 function App() {
@@ -27,36 +26,61 @@ function App() {
 
     return (
         <SongContext.Provider value={{currentSong, setCurrentSong, sound, setSound}}>
-            <div className="App">
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "start",
-                    padding: "3rem"
-                }}>
+            <div style={{ display: "flex", flexFlow: "column", height: "100%"}}>
+
+                <div style={{flex: "1 1 auto"}}>
                     <div style={{
                         display: "flex",
-                        padding: 2,
-                        marginBottom: '4px',
-                        borderBottom: "1px solid gray"
+                        gap: 10,
+                        padding: 10,
+                        height: "100%",
                     }}>
-                        <div
-                            style={{
-                                flex: 3,
-                            }}>
-                            Song
+                        <div className="contentTile" style={{flex: 3}}>
+                            <div className="buttonLink" style={{marginBottom: "1rem"}}>
+                                <HomeOutlined style={{fontSize: 30}}/> Home
+                            </div>
+                            <div className="buttonLink">
+                                <Search style={{fontSize: 30}}/> Search
+                            </div>
                         </div>
                         <div
+                            className="contentTile"
                             style={{
-                                flex: 1,
+                                display: "flex",
+                                flex: 6,
+                                flexDirection: "column",
+                                justifyContent: "start",
                             }}>
-                            Album
+                            <div style={{
+                                display: "flex",
+                                padding: 2,
+                                marginBottom: '4px',
+                                borderBottom: "1px solid gray"
+                            }}>
+                                <div
+                                    style={{
+                                        flex: 3,
+                                    }}>
+                                    Song
+                                </div>
+                                <div
+                                    style={{
+                                        flex: 1,
+                                    }}>
+                                    Album
+                                </div>
+                            </div>
+                            {songs.map(song => <SongCard key={song.id} song={song}/>)}
+                        </div>
+                        <div className="contentTile" style={{flex: 3}}>
+
                         </div>
                     </div>
-                    {songs.map(song => <SongCard key={song.id} song={song}/>)}
                 </div>
-                {<MediaPlayer/>}
+                <MediaPlayer/>
             </div>
+
+
         </SongContext.Provider>
     );
 }
