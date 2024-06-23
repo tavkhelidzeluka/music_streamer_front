e34c9a9b7432ee1093d965f901d2e09e18cc97fb
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-06-23 21:32:24 +0400

    Fix main content tiles

--- src/App.js
+++ src/App.js
@@ -26,55 +26,52 @@ function App() {
 
     return (
         <SongContext.Provider value={{currentSong, setCurrentSong, sound, setSound}}>
-            <div style={{ display: "flex", flexFlow: "column", height: "100%"}}>
+            <div style={{display: "flex", flexFlow: "column", height: "100%"}}>
+                <div style={{
+                    display: "flex",
+                    gap: 10,
+                    padding: 10,
+                    flex: "1 1 auto"
+                }}>
+                    <div className="contentTile" style={{flex: 3}}>
+                        <div className="buttonLink" style={{marginBottom: "1rem"}}>
+                            <HomeOutlined style={{fontSize: 30}}/> Home
+                        </div>
+                        <div className="buttonLink">
+                            <Search style={{fontSize: 30}}/> Search
+                        </div>
+                    </div>
+                    <div
+                        className="contentTile"
+                        style={{
+                            display: "flex",
+                            flex: 6,
+                            flexDirection: "column",
+                            justifyContent: "start",
+                        }}>
+                        <div style={{
+                            display: "flex",
+                            padding: 2,
+                            marginBottom: '4px',
+                            borderBottom: "1px solid gray"
+                        }}>
+                            <div
+                                style={{
+                                    flex: 3,
+                                }}>
+                                Song
+                            </div>
+                            <div
+                                style={{
+                                    flex: 1,
+                                }}>
+                                Album
+                            </div>
+                        </div>
+                        {songs.map(song => <SongCard key={song.id} song={song}/>)}
+                    </div>
+                    <div className="contentTile" style={{flex: 3}}>
 
-                <div style={{flex: "1 1 auto"}}>
-                    <div style={{
-                        display: "flex",
-                        gap: 10,
-                        padding: 10,
-                        height: "100%",
-                    }}>
-                        <div className="contentTile" style={{flex: 3}}>
-                            <div className="buttonLink" style={{marginBottom: "1rem"}}>
-                                <HomeOutlined style={{fontSize: 30}}/> Home
-                            </div>
-                            <div className="buttonLink">
-                                <Search style={{fontSize: 30}}/> Search
-                            </div>
-                        </div>
-                        <div
-                            className="contentTile"
-                            style={{
-                                display: "flex",
-                                flex: 6,
-                                flexDirection: "column",
-                                justifyContent: "start",
-                            }}>
-                            <div style={{
-                                display: "flex",
-                                padding: 2,
-                                marginBottom: '4px',
-                                borderBottom: "1px solid gray"
-                            }}>
-                                <div
-                                    style={{
-                                        flex: 3,
-                                    }}>
-                                    Song
-                                </div>
-                                <div
-                                    style={{
-                                        flex: 1,
-                                    }}>
-                                    Album
-                                </div>
-                            </div>
-                            {songs.map(song => <SongCard key={song.id} song={song}/>)}
-                        </div>
-                        <div className="contentTile" style={{flex: 3}}>
-
-                        </div>
                     </div>
                 </div>
                 <MediaPlayer/>
