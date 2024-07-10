138f4ce4d14bc095f922252b0ce38b64c9135010
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-07-10 12:25:52 +0400

    Add albums to main page

--- src/views/SongList.js
+++ src/views/SongList.js
@@ -1,11 +1,12 @@
 import {useEffect, useState} from "react";
 import {config} from "../config";
 import {SongCard} from "../components/SongCard";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {APIClientSecure} from "../api";
 
 export const SongList = () => {
     const [songs, setSongs] = useState([]);
+    const [albums, setAlbums] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -19,44 +20,75 @@ export const SongList = () => {
             }
         };
 
+        const fetchAlbums = async () => {
+            try {
+                const response = await APIClientSecure.get(config.api.album.list);
+                const data = await response.data;
+                setAlbums(data);
+            } catch (error) {
+                navigate("/sign/in/");
+            }
+        }
+
         fetchSongs();
+        fetchAlbums();
     }, []);
 
     return (
         <>
             <div style={{
                 display: "flex",
-                padding: 6,
-                marginBottom: '1rem',
-                borderBottom: "1px solid gray"
+                flexWrap: "wrap",
+                marginBottom: "1rem"
             }}>
-                <div
-                    style={{
-                        flex: 0.2,
-                        textAlign: "center"
-                    }}>
-                    #
-                </div>
-                <div
-                    style={{
-                        flex: 3,
-                    }}>
-                    Song
-                </div>
-                <div
-                    style={{
-                        flex: 1,
-                    }}>
-                    Album
-                </div>
-                <div
-                    style={{
-                        flex: 0.2,
-                    }}>
+                {albums.map(album => {
+                    return (
+                        <div key={album.id}
+                             style={{width: "50%"}}
+                             className="albumCard"
+                             onClick={() => navigate(`/album/${album.id}/`)}>
+                            <img src={album.cover} width={50} style={{ borderRadius: "6px", marginRight: "1rem"}}/>
+                            {album.title}
+                        </div>
+                    )
+                })}
 
-                </div>
             </div>
-            {songs.map((song, i) => <SongCard key={song.id} song={song} number={i + 1}/>)}
+            <div>
+                <div style={{
+                    display: "flex",
+                    padding: 6,
+                    marginBottom: '1rem',
+                    borderBottom: "1px solid gray"
+                }}>
+                    <div
+                        style={{
+                            flex: 0.2,
+                            textAlign: "center"
+                        }}>
+                        #
+                    </div>
+                    <div
+                        style={{
+                            flex: 3,
+                        }}>
+                        Song
+                    </div>
+                    <div
+                        style={{
+                            flex: 1,
+                        }}>
+                        Album
+                    </div>
+                    <div
+                        style={{
+                            flex: 0.2,
+                        }}>
+
+                    </div>
+                </div>
+                {songs.map((song, i) => <SongCard key={song.id} song={song} number={i + 1}/>)}
+            </div>
         </>
     );
 };
